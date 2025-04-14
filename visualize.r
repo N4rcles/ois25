bc7947f7e9e16d541a81d017df52a5ba69a35c5d
ch-unine-057226 <ch-unine-057226@noto.epfl.ch>
2025-04-14 20:38:35 +0200

    ajout de main.tex

---
+++ visualize.r
@@ -0,0 +1,8 @@
+x <- c(1,4,5,8,9)
+y <- c(3,6,8,13)
+boxplot(x, y,
+        main = "y en fonction de x",
+        xlab = "Valeur de x",
+        ylab = "Valeur de y",
+        col = c("lightblue", "lightgreen"))
+
